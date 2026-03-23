6d9c658def99942fab05295fcd453449edcb80c9
Stiven Macea <macea351@gmail.com>
2026-03-23 01:29:05 -0500

    creacion de la primera rama project-bootstrap

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        ShowMainMenu();
+    }
+
+    static void Pause()
+    {
+        Console.WriteLine("\nPresione una tecla...");
+        Console.ReadKey();
+    }
+
+    static void ActionMsg(string msg)
+    {
+        Console.WriteLine("\n[INFO] " + msg);
+        Pause();
+    }
+
+    static string GetOption()
+    {
+        Console.Write("\nSeleccione una opción: ");
+        return Console.ReadLine();
+    }
+
+    static void ShowMainMenu()
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.Clear();
+          {
+            Console.WriteLine("===== SISTEMA BIBLIOTECA =====");
+          }
+        }
+    }
+}
