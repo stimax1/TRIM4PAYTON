44629b2a94c3922f4c2945d834bb53488218905f
Stiven Macea <macea351@gmail.com>
2026-03-23 01:35:16 -0500

    creacion de la segunda rama main-menu-navegation

--- Program.cs
+++ Program.cs
@@ -32,9 +32,26 @@ class Program
         while (!exit)
         {
             Console.Clear();
-          {
             Console.WriteLine("===== SISTEMA BIBLIOTECA =====");
-          }
-        }
-    }
+            Console.WriteLine("1. Libros");
+            Console.WriteLine("2. Usuarios");
+            Console.WriteLine("3. Préstamos");
+            Console.WriteLine("4. Búsquedas y reportes");
+            Console.WriteLine("5. Guardar / Cargar");
+            Console.WriteLine("6. Salir");
+
+            switch (GetOption())
+            {
+                case "1": ShowBooksMenu(); break;
+                case "2": ShowUsersMenu(); break;
+                case "3": ShowLoansMenu(); break;
+                case "4": ShowSearchReportsMenu(); break;
+                case "5": ShowPersistenceMenu(); break;
+                case "6": exit = ConfirmExit(); break;
+                default: ActionMsg("Opción inválida"); break;
+            }
+
+          
+        }   
+    } 
 }
